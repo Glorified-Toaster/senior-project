d92599fb085f85f3262840c206a844120f5289f2
Glorified-Toaster <alhassanoday.ffxiv@gmail.com>
2026-03-22 02:02:08 +0300

    code question is working

--- internal/domain/question.go
+++ internal/domain/question.go
@@ -7,11 +7,12 @@ import (
 )
 
 type Question struct {
-	ID           uuid.UUID  `json:"id"`
-	ExamID       uuid.UUID  `json:"exam_id"`
-	QuestionText string     `json:"question_text"`
-	Marks        int        `json:"marks"`
-	CreatedAt    time.Time  `json:"created_at"`
-	UpdatedAt    time.Time  `json:"updated_at"`
-	DeletedAt    *time.Time `json:"deleted_at"`
+	ID            uuid.UUID  `json:"id"`
+	ExamID        uuid.UUID  `json:"exam_id"`
+	QuestionTitle string     `json:"question_title"`
+	QuestionText  string     `json:"question_text"`
+	Marks         int        `json:"marks"`
+	CreatedAt     time.Time  `json:"created_at"`
+	UpdatedAt     time.Time  `json:"updated_at"`
+	DeletedAt     *time.Time `json:"deleted_at"`
 }
